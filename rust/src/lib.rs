e318deb7092e427cf9837976aa39e30a6a6e437b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-07-07 22:58:59 -0400

    tweak rust test infra to use nextest and llvm cov to be compatible with stable instead of nightly

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,4 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Example {
     pub stuff: String,
 }
@@ -8,5 +6,29 @@ pub struct Example {
 impl Example {
     pub fn new(value: String) -> Self {
         Example { stuff: value }
+    }
+}
+
+/**********************************/
+#[cfg(test)]
+mod example_tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let e = Example::new(String::from("test"));
+        assert_eq!(e.stuff, String::from("test"));
+    }
+
+    #[test]
+    fn test_clone_and_eq() {
+        let e = Example::new(String::from("test"));
+        assert_eq!(e, e.clone());
+    }
+
+    #[test]
+    fn test_debug() {
+        let e = Example::new(String::from("test"));
+        assert_eq!(format!("{e:?}"), "Example { stuff: \"test\" }");
     }
 }
